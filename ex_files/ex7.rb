89dba41d909fee44dc7e069329cf74dfe5444cec
Y'Eli Buonya <ybuonya748@southseattle.edu>
2016-07-15 03:44:17 +0000

    add study drills

--- ex_files/ex7.rb
+++ ex_files/ex7.rb
@@ -1,8 +1,14 @@
+# prints string
 puts "Mary had a little lamb."
+# prints another string
 puts "Its fleece was white as #{'snow'}."
+# and another string
 puts "And everywhere that Mary went."
+# and this one prints out a string of 10 '.' 's
 puts "." * 10 # what'd I do?
 
+# assigned a string to each variable
+#assign C to variable end1... and so on and so forth
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -19,3 +25,6 @@ end12 = "r"
 # watch the print vs. puts on this line what's it do?
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
+
+# print will not display on a newline
+# puts will start a new line
