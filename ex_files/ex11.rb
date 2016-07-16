86a378b5e41b8950ff572327bb1ed106304e90b1
Y'Eli Buonya <ybuonya748@southseattle.edu>
2016-07-16 01:21:47 +0000

    add study drills

--- ex_files/ex11.rb
+++ ex_files/ex11.rb
@@ -10,3 +10,13 @@ puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 # the gets method will take user input as a string
 # the chomp method which is applied next will remove any carriage return characters
 # or newlines 
+
+print "What is your favorite movie? "
+movie = gets.chomp
+print "What is your favorite food? "
+food = gets.chomp
+print "How much wood, would a wood chuck chuck, if a wood chuck could chuck wood? "
+chuck_norris = gets.chomp
+
+puts "You like to eat #{food}, and watch #{movie}!"
+puts "#{chuck_norris} What?! That's insane!"
