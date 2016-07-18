e3161d14bf764c3f6f2d3e06e552a949350a2ced
Y'Eli Buonya <ybuonya748@southseattle.edu>
2016-07-18 23:45:37 +0000

    try study drills

--- ex_files/ex15.rb
+++ ex_files/ex15.rb
@@ -1,11 +1,11 @@
 # assigns the first argument to variable filename
 filename = ARGV.first
 # takes argument 'filename', opens it, and assigns it to variable txt
-txt = open(filename)
+#txt = open(filename)
 # prints a string with the variable 'filename'
-puts "Here's your file #{filename}:"
+#puts "Here's your file #{filename}:"
 # the read method is used here on the variable txt
-puts txt.read
+#puts txt.read
 # prints a string and waits for user input
 print "Type the filename again: "
 file_again = $stdin.gets.chomp
