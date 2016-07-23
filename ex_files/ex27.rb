a014f5873cad4154cdee29e5124d95e73b8d1da2
Y'Eli Buonya <ybuonya748@southseattle.edu>
2016-07-23 00:25:18 +0000

    add ex27.rb

---
+++ ex_files/ex27.rb
@@ -0,0 +1,8 @@
+# make a table and memorize boolean logic everyday and commit it to memory
+=begin 
+today is 22-July-2016, and i'm handwriting this in my notebook 
+instead of typing it in to this file.
+I will keep tracking by updating this file every instance that I write down
+the tables in my notebook, and I will commit it to memory and recite it aloud
+twice a day, once in the morning, and once in the evening before retiring to bed.
+=end
