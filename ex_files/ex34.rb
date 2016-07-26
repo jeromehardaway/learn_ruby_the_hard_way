180c5896353a26727b684816d1696073908f9ace
Y'Eli Buonya <ybuonya748@southseattle.edu>
2016-07-26 02:29:31 +0000

    add ex34.rb

---
+++ ex_files/ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals[1]# 1. The animal at 1 is 'ruby'
+puts animals[2]# 2. The third (3rd) animal is at index 2 peacock
+puts animals[0]# 3. The first (1st) animal is at index 0 bear
+puts animals[3]# 4. The animal at 3 is 'kangaroo'
+puts animals[4]# 5. the fifth (5th) animal is at index 4 whale
+puts animals[2]# 6. The animal at 2 is 'peacock'
+puts animals[5]# 7. The sixth (6th) animal is at index 5 platypus
+puts animals[4]# 8. The animal at 4 is 'whale'
